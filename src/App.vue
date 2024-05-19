9999eab7f83a1acdbb794ce7af7a1488a04aa73a
Rinat N <rinat-n@auto.kz>
2024-05-19 17:58:48 +0500

    dev: configure firebase

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+onMounted(() => {
+  const firebaseConfig = {
+            apiKey: "AIzaSyDdDForfHHAz5mY0nvuKP6YWfCgmtcj37U",
+            authDomain: "fir-sample-898fd.firebaseapp.com",
+            projectId: "fir-sample-898fd",
+            storageBucket: "fir-sample-898fd.appspot.com",
+            messagingSenderId: "343313224769",
+            appId: "1:343313224769:web:a0f794e57b44073be7e2c0"
+        };
+        const app = initializeApp(firebaseConfig);
+        getAnalytics(app);
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>Здесь будет сайт приложения</div>
 </template>
 
 <style scoped>
